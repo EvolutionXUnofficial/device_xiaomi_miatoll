8886db6c0c4bcdd1c1d2fd19f19fdf6a52b31fab
Sushrut Gupta <75667593+Sushrut1101@users.noreply.github.com>
2021-06-04 14:27:57 +0530

    Add Gapps Stuff

--- evolution_miatoll.mk
+++ evolution_miatoll.mk
@@ -10,15 +10,16 @@ $(call inherit-product, device/xiaomi/miatoll/device.mk)
 # Inherit some common Evolution-X stuff
 $(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
-
-# Bootanimation Resolution
+#Gapps & Stuff
 TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_STOCK_ARCORE := true
 
 # Device identifier
 PRODUCT_NAME := evolution_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := Miatoll
+PRODUCT_MODEL := Redmi Note 9 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
